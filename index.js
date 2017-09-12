528be086c82f04b5ae9f9069b31e936831b92b4e
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-12 20:09:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber(array, name){
+  if (array.length === 0){
+    array.push(name);
+  }
+  else{
+    array.push(name);
+  }
+  return "Welcome, " + name + ". You are number " + (i+1);
+}
